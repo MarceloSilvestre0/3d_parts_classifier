5cde46da9a9b8ebcdec35f213defa83a764e0af3
MarceloSilvestre0 <marcelosilvestrefilho@outlook.com>
2025-10-13 19:49:13 -0300

    Criando a 'Data Preparation'

---
+++ src/data_process/data_preparation.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+list_bounding = [{'NM_PART': 'bishop_ref.stl', 'WIDTH': 40.0, 'HEIGHT': 40.0, 'HEIGHT': 76.0}]
+list_classes = ['Bishop', 'King', 'Knight', 'Pawn', 'Bishop', 'Rook']
+
+colunas = ["NM_PART",
+           "WIDTH", "HEIGHT", "LENGTH"]
+
+list_bounding_data = pd.DataFrame(list_bounding, columns=colunas)
+print(list_bounding_data)
